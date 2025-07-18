58f7a47679f30261da6a7cb91777a9498440731d
tobinio <Tobias.frischmann1@gmail.com>
2025-07-18 16:58:39 +0200

    add tailwind

---
+++ app/pages/api.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">monopoly api</h1>
+
+        <div>
+            <div>
+                <button>send Player card</button>
+            </div>
+            <div>
+                <button>send Property card</button>
+            </div>
+            <div>
+                <button>send Jail card</button>
+            </div>
+        </div>
+    </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,28 @@
 <script setup lang="ts">
-
-import {io} from "socket.io-client";
-import type {Color} from "~/utils/types";
+import { io } from "socket.io-client";
+import type { Color } from "~/utils/types";
 
 const socket = io("/", {
-  path: "/api/socket.io",
+    path: "/api/socket.io",
 });
 
 const game = new Game();
 
 socket.on("game:playerCard", (color: Color) => {
-  game.activatePlayerCard(color);
+    game.activatePlayerCard(color);
 });
 
 socket.on("game:propertyCard", (id: number) => {
-  game.activatePropertyCard(id);
+    game.activatePropertyCard(id);
 });
 
 socket.on("game:jailCard", () => {
-  game.outOfJail();
-})
-
+    game.outOfJail();
+});
 </script>
 
 <template>
-  <div>
-    <h1>monopoly works</h1>
-  </div>
+    <div>
+        <h1 class="text-2xl font-bold">monopoly works</h1>
+    </div>
 </template>
