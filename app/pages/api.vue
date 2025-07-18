3fbfba06867dafd0dfa639778b282ea85f37fe92
tobinio <Tobias.frischmann1@gmail.com>
2025-07-18 17:07:36 +0200

    simple debugging ui

--- app/pages/api.vue
+++ app/pages/api.vue
@@ -1,18 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let color = ref("blue");
+
+function onPlayerCard(color: string) {
+    $fetch("/api/playerCard/", {
+        method: "POST",
+        body: JSON.stringify({ color }),
+    });
+}
+
+let id = ref(0);
+
+function onPropertyCard(id: number) {
+    $fetch("/api/property/", {
+        method: "POST",
+        body: JSON.stringify({ id }),
+    });
+}
+
+function onJailCard() {
+    $fetch("/api/jailCard/", {
+        method: "POST",
+        body: JSON.stringify({}),
+    });
+}
+</script>
 
 <template>
     <div>
         <h1 class="text-2xl font-bold">monopoly api</h1>
 
-        <div>
-            <div>
-                <button>send Player card</button>
+        <div class="flex gap-2 flex-col">
+            <div class="flex gap-2">
+                <button
+                    class="cursor-pointer border px-2 rounded"
+                    @click="onPlayerCard(color)"
+                >
+                    send Player card
+                </button>
+                <select class="border px-2 rounded" v-model="color">
+                    <option value="red">Red</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                    <option value="yellow">Yellow</option>
+                </select>
+            </div>
+            <div class="flex gap-2">
+                <button
+                    class="cursor-pointer border px-2 rounded"
+                    @click="onPropertyCard(id)"
+                >
+                    send Property card
+                </button>
+                <input class="border px-2 rounded" v-model="id" type="number" />
             </div>
             <div>
-                <button>send Property card</button>
-            </div>
-            <div>
-                <button>send Jail card</button>
+                <button
+                    class="cursor-pointer border px-2 rounded"
+                    @click="onJailCard"
+                >
+                    send Jail card
+                </button>
             </div>
         </div>
     </div>
